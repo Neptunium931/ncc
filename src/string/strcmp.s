7d4840f8f668584854bd1ea7519fe032e7282711
neptumiun <tymothe.billerey@gmail.com>
2026-03-02 15:51:21 +0100

    feat(string) add strcmp

---
+++ src/string/strcmp.s
@@ -0,0 +1,78 @@
+# Copyright (c) 2026, Tymothé BILLEREY <tymothe_billerey@fastmail.fr>
+# See end of file for extended copyright information.
+.intel_syntax noprefix
+
+.global strcmp
+
+strcmp:
+	push rbp
+	mov  rbp, rsp
+	push rbx
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+
+# rdi char *s1
+# rsi char *s2
+# rax int
+strcmp.body:
+	cmp   byte ptr [rdi], 0
+	je    strcmp.endS1
+	cmp   byte ptr [rsi], 0
+	je    strcmp.endS2
+	movzx rax, byte ptr [rdi]
+	movzx rbx, byte ptr [rsi]
+	cmp   rax, rbx
+	jne   strcmp.end
+	inc   rdi
+	inc   rsi
+	jmp   strcmp.body
+
+strcmp.end:
+	movzx rax, byte ptr [rdi]
+	movzx rbx, byte ptr [rsi]
+	sub   rax, rbx
+
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop rbx
+	pop rbp
+	ret
+
+# This file is part of ncc.
+#
+# BSD 3-Clause License
+#
+# Copyright (c) 2026, Tymothé BILLEREY <tymothe_billerey@fastmail.fr>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice
+# this
+# list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
